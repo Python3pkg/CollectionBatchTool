fc8d9f995b0abd66da9b2db02dc3588f5e99e66a
jmenglund <jan.markus.englund@gmail.com>
2016-03-14 16:08:25 +0100

    Add utf-8 support for long description

--- setup.py
+++ setup.py
@@ -2,15 +2,18 @@ from setuptools import setup, find_packages
 from os.path import join, dirname
 import sys
 
+
 if sys.version_info.major < 3:
     print("Sorry, currently only Python 3 is supported!")
     sys.exit(1)
+
 
 setup(
     name = 'CollectionBatchTool',
     version=__import__('collectionbatchtool').__version__,
     description = 'batch import and export of Specify data',
-    long_description = open(join(dirname(__file__), 'README.rst')).read(),
+    long_description = open(
+        join(dirname(__file__), 'README.rst'), encoding='utf-8').read(),
     packages = find_packages(),
     py_modules = ['collectionbatchtool', 'specifymodels'],
     install_requires = ['pandas>=0.16', 'peewee>=2.6', 'pymysql'],
